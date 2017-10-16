860c0fb3d8bb51da663e86d1ecbf8c17ba655b00
vdominika <vdominika@yahoo.com>
2017-10-16 10:14:15 +0200

    Questions created

---
+++ src/Questions.java
@@ -0,0 +1,17 @@
+//What is inheritance? When would you use it? [2p]
+//
+//        Inheritance can be defined as the process where one class acquires the properties (methods and fields) of another. The class which inherits the properties of other is known as subclass (derived class, child class) and the class whose properties are inherited is known as superclass (base class, parent class).
+//
+//        With the use of inheritance the information is easily manageable in a hierarchical system.
+//
+//
+//        Name each building block of a method! [2p]
+//
+//        1. Statement
+//        2. Variable type
+//        3.
+//        4
+//        5
+//        6
+//        7
+//        8
